f2748abdafcecb3f96fdb44edb986e0a45ab1f97
wzb <1006736301@qq.com>
2019-05-24 16:41:12 +0800

    修改打包出现的相对路径不正确BUG

---
+++ build/precache-manifest.47059775e3454d46f78e5e3be640045f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "36656b9c2afa4b6c07fe",
+    "url": "./static/js/main.5c6c7e9d.chunk.js"
+  },
+  {
+    "revision": "9fb35ef15739cdefd9aa",
+    "url": "./static/js/2.04b2c69e.chunk.js"
+  },
+  {
+    "revision": "36656b9c2afa4b6c07fe",
+    "url": "./static/css/main.2505876a.chunk.css"
+  },
+  {
+    "revision": "9fb35ef15739cdefd9aa",
+    "url": "./static/css/2.54ef08de.chunk.css"
+  },
+  {
+    "revision": "7777424f5c75017e120c244e927acc85",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ca05243e174a526fc09d820fa517c21.js"
+  "./precache-manifest.47059775e3454d46f78e5e3be640045f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
